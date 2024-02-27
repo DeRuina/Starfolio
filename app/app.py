4e8811c956e258f0c16e481d2b5acf3d8cf4bfe0
Dean Ruina <ruinadd@gmail.com>
2024-02-27 09:23:56 +0200

    send the client_id and recieving the code work

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from dotenv import load_dotenv
+from fastapi.responses import RedirectResponse
+import os
 
 load_dotenv()
 
 app = FastAPI()
 
-@app.get("/", tags=["ROOT"])
-async def root() ->dict:
-  return {"name": "Dean"}
+@app.get("/login")
+async def github_login(request: Request) -> RedirectResponse:
+  client_id = os.getenv("GITHUB_CLIENT_ID")
+  redirect_uri = request.url_for("authorize")
+  return RedirectResponse(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}")
+
+
+@app.get("/authorize")
+async def authorize(code: str):
+  print(code)
