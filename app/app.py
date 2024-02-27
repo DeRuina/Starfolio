7b6fa1deaeb5a5927ab30fb16ee7dfc404f16891
Dean Ruina <ruinadd@gmail.com>
2024-02-27 11:35:35 +0200

    comments added & state parameter to prevent CSRF attacks

--- app/app.py
+++ app/app.py
@@ -1,24 +1,42 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Cookie
 from dotenv import load_dotenv
 from fastapi.responses import RedirectResponse
 import os
+import binascii
 
-load_dotenv()
 
-app = FastAPI()
+load_dotenv()  # Load environment variables from .env file for configuration
 
+
+app = FastAPI() # FastAPI instance for building the web application
+
+
+# Redirect the user to /login
 @app.get("/", tags=["ROOT"])
 async def root(request: Request) -> RedirectResponse:
   return RedirectResponse(request.url_for("github_login"))
 
-
+# Redirect the user to GitHub's authorization page
 @app.get("/login", tags=["LOGIN"])
 async def github_login(request: Request) -> RedirectResponse:
   client_id = os.getenv("GITHUB_CLIENT_ID")
-  redirect_uri = request.url_for("authorize")
-  return RedirectResponse(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}")
+  state = binascii.hexlify(os.urandom(16)).decode() # To prevent CSRF attacks
+  response = RedirectResponse(f"https://github.com/login/oauth/authorize?client_id={client_id}&state={state}")
+  response.set_cookie(key="state", value=state, secure=True)
+  return response
 
+# Exchange the authorization code for an access token
+@app.get("/authorize", tags=["AUTHORIZE"])
+async def authorize(request: Request, state: str = Cookie(None)):
+  if state is None:
+    raise HTTPException(status_code=400, detail="State cookie not found")
+  
+  state_returned = request.query_params.get("state")
+  if state_returned != state:
+    raise HTTPException(status_code=400, detail="Invalid state parameter")
 
-@app.get("/authorize")
-async def authorize(code: str):
-  print(code)
+  code = request.query_params.get("code")
+  if not code:
+    raise HTTPException(status_code=400, detail="No code received")
+  
+  
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
 
+# Starting the ASGI server at runtime if this script is executed directly and not imported
 if __name__ ==  "__main__":
   uvicorn.run("app.app:app", reload = True)
