6639f700d945aa5e6fa5b51b1cd1a3e612fe11f7
Dean Ruina <ruinadd@gmail.com>
2024-02-28 09:45:39 +0200

    fetching reops and creating helper functions

--- app/app.py
+++ app/app.py
@@ -27,6 +27,40 @@ async def github_login(request: Request) -> RedirectResponse:
   return response
 
 # Exchange the authorization code for an access token
+async def exchange_code_for_token(code:str, client_id: str, client_secret: str) -> str:
+  params = {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "code": code
+  }
+  headers = {"Accept": "application/json"}
+  # Found that is better to use httpx.AsyncClient() for asynchronous requests 
+  # from this Medium post: https://medium.com/featurepreneur/what-is-httpx-a0071df05c4a
+  async with httpx.AsyncClient() as client:
+    response = await client.post(url="https://github.com/login/oauth/access_token", params=params, headers=headers)
+    if response.status_code == 200:
+      token_data = response.json()
+      access_token = token_data.get("access_token")
+      if not access_token:
+        raise HTTPException(status_code=400, detail="Failed to obtain access token")
+      return access_token
+    else:
+      raise HTTPException(status_code=response.status_code, detail="Failed to make request to GitHub OAuth")
+    
+# Fetch starred repositories
+async def get_starred_repositories(access_token: str) -> list:
+  async with httpx.AsyncClient() as client:
+    headers = {"Accept": "application/json", "Authorization": f"Bearer {access_token}"}
+    response = await client.get(url="https://api.github.com/user/starred", headers=headers)
+    if response.status_code == 200:
+      repos = response.json()
+      public_repos = [repo for repo in repos if not repo["private"]]
+      return public_repos
+    else:
+      raise HTTPException(status_code=response.status_code, detail="Failed to make Get request")
+  
+
+# Perform checks and call helper functions
 @app.get("/authorize", tags=["AUTHORIZE"])
 async def authorize(request: Request, state: str = Cookie(None)):
   if state is None:
@@ -42,24 +76,9 @@ async def authorize(request: Request, state: str = Cookie(None)):
   
   client_id = os.getenv("GITHUB_CLIENT_ID")
   client_secret = os.getenv("GITHUB_CLIENT_SECRET")
-  params = {
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "code": code
-  }
-  headers = {"Accept": "application/json"}
 
-  # Found that is better to use httpx.AsyncClient() for asynchronous requests 
-  # from this Medium post: https://medium.com/featurepreneur/what-is-httpx-a0071df05c4a
-  async with httpx.AsyncClient() as client:
-    response = await client.post(url="https://github.com/login/oauth/access_token", params=params, headers=headers)
-    if response.status_code == 200:
-      token_data = response.json()
-      access_token = token_data.get("access_token")
-      if not access_token:
-        raise HTTPException(status_code=400, detail="Failed to obtain access token")
-    else:
-      raise HTTPException(status_code=response.status_code, detail="Failed to make request to GitHub OAuth")
-    
+  access_token = await exchange_code_for_token(code, client_id, client_secret)
+  public_repos = await get_starred_repositories(access_token)
+  return public_repos
   
   
