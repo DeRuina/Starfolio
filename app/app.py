ee2fe4925ddbb7ca0abc9269237ee4e859875bdb
Dean Ruina <ruinadd@gmail.com>
2024-02-28 09:05:25 +0200

    exchange for access token done

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from fastapi.responses import RedirectResponse
 import os
 import binascii
+import httpx
 
 
 load_dotenv()  # Load environment variables from .env file for configuration
@@ -30,7 +31,7 @@ async def github_login(request: Request) -> RedirectResponse:
 async def authorize(request: Request, state: str = Cookie(None)):
   if state is None:
     raise HTTPException(status_code=400, detail="State cookie not found")
-  
+
   state_returned = request.query_params.get("state")
   if state_returned != state:
     raise HTTPException(status_code=400, detail="Invalid state parameter")
@@ -38,5 +39,25 @@ async def authorize(request: Request, state: str = Cookie(None)):
   code = request.query_params.get("code")
   if not code:
     raise HTTPException(status_code=400, detail="No code received")
+  
+  client_id = os.getenv("GITHUB_CLIENT_ID")
+  client_secret = os.getenv("GITHUB_CLIENT_SECRET")
+  params = {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "code": code
+  }
+  headers = {"Accept": "application/json"}
+
+  async with httpx.AsyncClient() as client:
+    response = await client.post(url="https://github.com/login/oauth/access_token", params=params, headers=headers)
+    if response.status_code == 200:
+      token_data = response.json()
+      access_token = token_data.get("access_token")
+      if not access_token:
+        raise HTTPException(status_code=400, detail="Failed to obtain access token")
+    else:
+      raise HTTPException(status_code=response.status_code, detail="Failed to make request to GitHub OAuth")
+    
   
   
