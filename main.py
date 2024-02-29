ee0061b31d71cd847a16f579b90c5a36d758eb8e
Dean Ruina <ruinadd@gmail.com>
2024-03-01 01:15:37 +0200

    reload off for deployment

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 # Starting the ASGI server at runtime if this script is executed directly and not imported
 if __name__ ==  "__main__":
-  uvicorn.run("app.app:app", reload = True)
+  uvicorn.run("app.app:app", reload = False)
